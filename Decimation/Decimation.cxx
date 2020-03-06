f4ac8329d3e25fd36837499603d5170d63d985ab
Andras Lasso <lasso@queensu.ca>
2020-03-06 13:44:20 -0500

    ENH: Add quadric decimation options
    Added VTK's quadric decimation filter and Sven Forstmann's fast quadric decimation filter implementation.
    Quadric filters provide much better shaped triangles, especially when large reduction ratio is requested.

--- Decimation/Decimation.cxx
+++ Decimation/Decimation.cxx
@@ -2,57 +2,139 @@
 
 // VTK Includes
 #include "vtkDecimatePro.h"
+#include "vtkNew.h"
+#include "vtkOBJReader.h"
+#include "vtkOBJWriter.h"
+#include "vtkPolyData.h"
+#include "vtkQuadricDecimation.h"
+#include "vtkSmartPointer.h"
+#include "vtkTriangleFilter.h"
 #include "vtkXMLPolyDataWriter.h"
 #include "vtkXMLPolyDataReader.h"
-#include "vtkSmartPointer.h"
-#include "vtkPolyData.h"
-#include "vtkTriangleFilter.h"
-#include "vtkNew.h"
+#include <vtksys/SystemTools.hxx>
 
+#include "Simplify.h" // FastQuadric method
 
+int main(int argc, char* argv[])
+{
+  PARSE_ARGS;
 
-int main (int argc, char * argv[])
- {
-   PARSE_ARGS;
+  std::string inputModelExt = vtksys::SystemTools::LowerCase(vtksys::SystemTools::GetFilenameLastExtension(inputModel));
+  std::string outputModelExt = vtksys::SystemTools::LowerCase(vtksys::SystemTools::GetFilenameLastExtension(outputModel));
 
-   try{
+  if (method == "FastQuadric")
+    {
+    if (inputModelExt != ".obj" || outputModelExt != ".obj")
+      {
+      std::cerr << "FastQuadric method only supports input/output mesh files in OBJ format." << std::endl;
+      return EXIT_FAILURE;
+      }
+    Simplify::load_obj(inputModel.c_str());
+    if ((Simplify::triangles.size() < 3) || (Simplify::vertices.size() < 3))
+      {
+      std::cerr << "Minimum 3 triangles are needed." << std::endl;
+      return EXIT_FAILURE;
+      }
+    int target_count = round((float)Simplify::triangles.size() * (1.0-reductionFactor));
+    if (target_count < 4)
+      {
+      std::cerr << "Object will not survive such extreme decimation." << std::endl;
+      return EXIT_FAILURE;
+      }
+    std::cout << "Input: " << Simplify::vertices.size() << " vertices,"
+      << Simplify::triangles.size() << " triangles (target " << target_count << ")" << std::endl;
+    int startSize = Simplify::triangles.size();
+    if (lossless)
+      {
+      Simplify::simplify_mesh_lossless(verbose);
+      }
+    else
+      {
+      Simplify::simplify_mesh(target_count, aggressiveness, verbose);
+      }
+    if (Simplify::triangles.size() >= startSize)
+      {
+      std::cerr << "Unable to reduce mesh." << std::endl;
+      return EXIT_FAILURE;
+      }
+    Simplify::write_obj(outputModel.c_str());
+    double achievedReduction = 1.0 - (double)Simplify::triangles.size() / (double)startSize;
+    std::cout << "Output: " << Simplify::vertices.size() << " vertices,"
+      << Simplify::triangles.size() << " triangles (" << achievedReduction << " reduction)" << std::endl;
+    return EXIT_SUCCESS;
+    }
 
-    vtkSmartPointer<vtkPolyData> inputPolyData;
-    vtkSmartPointer<vtkPolyData> polyData;
+  // VTK decimation filters
 
-
-    // Read the file
-    vtkNew<vtkXMLPolyDataReader> reader;
-    reader->SetFileName(inputVolume.c_str());
+  // Read the input model
+  vtkSmartPointer<vtkPolyData> inputPolyData;
+  if (inputModelExt == ".obj")
+    {
+    vtkNew<vtkOBJReader> reader;
+    reader->SetFileName(inputModel.c_str());
     reader->Update();
-    polyData = reader->GetOutput();
+    inputPolyData = reader->GetOutput();
+    }
+  else if (inputModelExt == ".vtp")
+    {
+    vtkNew<vtkXMLPolyDataReader> reader;
+    reader->SetFileName(inputModel.c_str());
+    reader->Update();
+    inputPolyData = reader->GetOutput();
+    }
+  else
+    {
+    std::cerr << "Input mesh is expected in OBJ or VTP file format." << std::endl;
+    return EXIT_FAILURE;
+    }
 
-    vtkNew<vtkTriangleFilter> triangles;
-    triangles->SetInputData(polyData);
-    triangles->Update();
-    // create poly data with triangle filter
-    inputPolyData = triangles->GetOutput();
+  // Triangulate input mesh
+  vtkNew<vtkTriangleFilter> triangles;
+  triangles->SetInputData(inputPolyData);
+  triangles->Update();
+  inputPolyData = triangles->GetOutput();
 
-    vtkNew<vtkDecimatePro> decimate;
-
+  vtkSmartPointer<vtkPolyData> outputPolyData;
+  if (method == "Quadric")
+    {
+    vtkNew<vtkQuadricDecimation> decimate;
     decimate->SetInputData(inputPolyData);
-    decimate->SetTargetReduction(Decimate);
-    decimate->SetBoundaryVertexDeletion(Boundary);
+    decimate->SetTargetReduction(reductionFactor);
+    //decimate->SetVolumePreservation(true);
+    decimate->Update();
+    outputPolyData = decimate->GetOutput();
+    }
+  else
+    {
+    vtkNew<vtkDecimatePro> decimate;
+    decimate->SetInputData(inputPolyData);
+    decimate->SetTargetReduction(reductionFactor);
+    decimate->SetBoundaryVertexDeletion(boundaryDeletion);
     decimate->PreserveTopologyOn();
     decimate->Update();
+    outputPolyData = decimate->GetOutput();
+    }
 
-    //Write to file
-    vtkNew<vtkXMLPolyDataWriter> writer;
-    writer->SetFileName(outputVolume.c_str());
-    writer->SetInputData(decimate->GetOutput());
+  //Write to file
+  if (outputModelExt == ".obj")
+    {
+    vtkNew<vtkOBJWriter> writer;
+    writer->SetFileName(outputModel.c_str());
+    writer->SetInputData(outputPolyData);
     writer->Update();
-  }
-  catch (int e)
-   {
-     cout << "An exception occurred. Exception Nr. " << e << '\n';
-     return EXIT_FAILURE;
-   }
+    }
+  else if (outputModelExt == ".vtp")
+    {
+    vtkNew<vtkXMLPolyDataWriter> writer;
+    writer->SetFileName(outputModel.c_str());
+    writer->SetInputData(outputPolyData);
+    writer->Update();
+    }
+  else
+    {
+    std::cerr << "Output mesh can be written in OBJ or VTP file format." << std::endl;
+    return EXIT_FAILURE;
+    }
 
   return EXIT_SUCCESS;
-
- }
+}
